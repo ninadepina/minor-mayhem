52f2c465bf4fb865081d40e9b3cea5680c7c3135
Nina Vens <nina.vens@hva.nl>
2023-05-17 12:55:46 +0200

    [added] validation

---
+++ scripts/validateUser.js
@@ -0,0 +1,107 @@
+const form = document.querySelector('#info_form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const studentnummerInput = document.querySelector('#studentnummer');
+
+form.setAttribute('novalidate', '');
+
+document.addEventListener('DOMContentLoaded', () => {
+	const inputs = document.querySelectorAll('.css-input');
+
+	inputs.forEach((input) => {
+		input.classList.remove('css-input');
+	});
+});
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	clearErrorMessages();
+
+	if (nameInput.value.trim() === '') showError(nameInput, 'Je kan dit veld niet leeg laten');
+
+	if (!isValidEmail(emailInput.value.trim())) {
+		if (emailInput.value.trim() === '') {
+			showError(emailInput, 'Je kan dit veld niet leeg laten');
+		} else if (!emailInput.value.includes('@')) {
+			showError(emailInput, 'Je mist een @-symbool in je e-mailadres');
+		} else if (!hasValidDomain(emailInput.value.trim())) {
+			showError(emailInput, 'Je mist een domein (bijv. .com of .nl) in het e-mailadres');
+		} else {
+			showError(emailInput, 'Weet je zeker dat je e-mailadres klopt? (bijv. jouwnaam@gmail.com)');
+		}
+	}
+
+	if (!isValidStudentNumber(studentnummerInput.value.trim())) {
+		const inputLength = studentnummerInput.value.trim().length;
+		const requiredLength = 9;
+
+		if (studentnummerInput.value.trim() === '') {
+			showError(studentnummerInput, 'Je kan dit veld niet leeg laten');
+		} else if (!containsOnlyDigits(studentnummerInput.value.trim())) {
+			showError(studentnummerInput, 'Je studentnummer mag alleen cijfers bevatten');
+		} else if (inputLength < requiredLength) {
+			const missingDigits = requiredLength - inputLength;
+			showError(
+				studentnummerInput,
+				`Je studentnummer bestaat uit 9 cijfers, je mist nu nog ${missingDigits} cijfer(s)`
+			);
+		} else {
+			showError(studentnummerInput, 'Weet je zeker dat je studentnummer klopt? (bijv. 500812345)');
+		}
+	}
+	if (isFormValid()) form.submit();
+});
+
+function showError(input, message) {
+	const errorDiv = input.parentNode.querySelector('.error');
+	errorDiv.innerText = message;
+	input.classList.add('err');
+}
+
+function clearErrorMessages() {
+	const errorDivs = document.querySelectorAll('.error');
+	errorDivs.forEach(function (errorDiv) {
+		errorDiv.innerText = '';
+	});
+}
+
+function isValidEmail(email) {
+	const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/;
+	return emailRegex.test(email);
+}
+function hasValidDomain(email) {
+	const parts = email.split('@');
+	if (parts.length === 2 && parts[1].includes('.')) {
+		return true;
+	}
+	return false;
+}
+
+function isValidStudentNumber(studentnummer) {
+	const studentnummerRegex = /^[56]\d{8}$/;
+	return studentnummerRegex.test(studentnummer);
+}
+function containsOnlyDigits(studentnummer) {
+	const letterRegex = /[a-zA-Z]/;
+	return !letterRegex.test(studentnummer);
+}
+
+function isFormValid() {
+	const errorDivs = document.querySelectorAll('.error');
+	let isValid = true;
+
+	errorDivs.forEach(function (errorDiv) {
+		if (errorDiv.innerText !== '') isValid = false;
+	});
+
+	const answers = {};
+
+	answers['name'] = nameInput.value;
+	answers['email'] = emailInput.value;
+	answers['studentnummer'] = studentnummerInput.value;
+
+	localStorage.setItem('user', JSON.stringify(answers));
+
+	return isValid;
+}
---
+++ scripts/validateVak.js
@@ -0,0 +1,105 @@
+const form = document.querySelector('.enq');
+const mainElement = form.closest('main').className;
+
+const savedAnswers = localStorage.getItem(mainElement);
+if (savedAnswers) {
+	const parsedAnswers = JSON.parse(savedAnswers);
+
+	const radios = document.querySelectorAll('input[type="radio"]');
+	radios.forEach(function (radio) {
+		const fieldName = radio.name;
+		const value = radio.value;
+		if (parsedAnswers.hasOwnProperty(fieldName) && parsedAnswers[fieldName] === value) {
+			radio.checked = true;
+		}
+	});
+
+	const textarea = document.getElementById('kladblok');
+	const kladblokValue = parsedAnswers['kladblok'] || '';
+	textarea.value = kladblokValue;
+}
+
+form.setAttribute('novalidate', '');
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	if (validateForm()) {
+		saveAnswers();
+		window.location.href = './dashboard.html';
+	} else {
+		scrollToFirstInvalidFieldset();
+	}
+});
+
+function validateForm() {
+	let isValid = true;
+
+	const fieldsets = document.querySelectorAll('fieldset');
+	fieldsets.forEach(function (fieldset) {
+		const errorElement = fieldset.querySelector('.error');
+		const radios = fieldset.querySelectorAll('input[type="radio"]');
+
+		let isChecked = false;
+		radios.forEach(function (radio) {
+			if (radio.checked) {
+				isChecked = true;
+			}
+		});
+
+		if (!isChecked && errorElement) {
+			fieldset.classList.add('err');
+			errorElement.textContent = 'Je bent deze vraag vergeten te beantwoorden..';
+			errorElement.classList.add('error_margin');
+			isValid = false;
+		} else {
+			if (errorElement) {
+				fieldset.classList.remove('err');
+				errorElement.textContent = '';
+				errorElement.classList.remove('error_margin');
+			}
+		}
+	});
+
+	return isValid;
+}
+
+function saveAnswers() {
+	const answers = {};
+
+	const radioButtons = document.querySelectorAll('input[type="radio"]:checked');
+	radioButtons.forEach(function (radioButton) {
+		const fieldName = radioButton.name;
+		const value = radioButton.value;
+		answers[fieldName] = value;
+	});
+
+	const textarea = document.getElementById('kladblok');
+	const kladblokValue = textarea.value.trim();
+	answers['kladblok'] = kladblokValue;
+
+	localStorage.setItem(mainElement, JSON.stringify(answers));
+}
+
+function scrollToFirstInvalidFieldset() {
+	const fieldsets = document.querySelectorAll('fieldset');
+	
+	for (let i = 0; i < fieldsets.length; i++) {
+		const fieldset = fieldsets[i];
+		const errorElement = fieldset.querySelector('.error');
+		const radios = fieldset.querySelectorAll('input[type="radio"]');
+		let isChecked = false;
+
+		radios.forEach(function (radio) {
+			if (radio.checked) {
+				isChecked = true;
+			}
+		});
+
+		if (!isChecked && errorElement) {
+			const yOffset = fieldset.getBoundingClientRect().top + window.pageYOffset - 40;
+			window.scrollTo({ top: yOffset, behavior: 'smooth' });
+			break;
+		}
+	}
+}
